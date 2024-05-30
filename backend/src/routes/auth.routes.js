404cf9ebb6aa254ee88c60f53cd765a3f039459a
Pablo Spector <pablo_spector@hotmail.com>
2024-05-30 20:35:37 -0300

    backend

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { login, register, logout } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
