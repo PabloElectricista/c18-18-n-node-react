364b95a6065c0b38eb0f59723c9f40f2b3c91aec
MauricioAguilarPardo90 <mauricioaguilarpardo@gmail.com>
2024-06-04 17:11:17 -0500

    Editar intancias doctor

--- backend/src/utils/intances-usecases.js
+++ backend/src/utils/intances-usecases.js
@@ -2,44 +2,29 @@ import prisma from "../infrastructure/prisma/prismaConfig.js";
 
 //Repositories
 import PatientPrismaRepository from "../adapters/repositories/patient-prisma-repository.js";
-
-//Usecases
-import PatientUseCases from "../application/usecases/patient-usecases.js";
-
-//Handlers
-import PatientHandler from "../adapters/http/user/patient-handler.js";
-
-//Intance-Repository
-const patientPrismaRepository = new PatientPrismaRepository(prisma);
-
-//Intances-usecases
-const patientUseCases = new PatientUseCases(patientPrismaRepository);
-
-//Intance-Handler
-const patientHandler = new PatientHandler(patientUseCases);
-
-export {patientHandler};
-
-
-//Repositories
 import DoctorPrismaRepository from "../adapters/repositories/doctor-prisma-repository.js";
 
 //Usecases
+import PatientUseCases from "../application/usecases/patient-usecases.js";
 import DoctorUseCases from "../application/usecases/doctor-usecases.js"
 
 //Handlers
+import PatientHandler from "../adapters/http/user/patient-handler.js";
 import Doctorhandler from "../adapters/http/user/doctor-handler.js";
 
 //Intance-Repository
+const patientPrismaRepository = new PatientPrismaRepository(prisma);
 const doctorPrismaRepository = new DoctorPrismaRepository(prisma);
 
 //Intances-usecases
-const doctorUseCases = new DoctorUseCases(patientPrismaRepository);
-
+const patientUseCases = new PatientUseCases(patientPrismaRepository);
+const doctorUseCases = new DoctorUseCases(doctorPrismaRepository);
 //Intance-Handler
-const doctorHandler = new Doctorhandler(patientUseCases);
+const patientHandler = new PatientHandler(patientUseCases);
+const doctorHandler = new Doctorhandler(doctorUseCases);
 
-export  {doctorHandler};
+export {patientHandler};
+export {doctorHandler};
 
 
 
