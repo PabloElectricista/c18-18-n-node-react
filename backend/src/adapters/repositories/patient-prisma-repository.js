03fa2e8d63eaa675db8c7b874af61bb9d14815fb
MauricioAguilarPardo90 <mauricioaguilarpardo@gmail.com>
2024-06-04 01:18:17 -0500

    Se crea repositorio de paciente

--- backend/src/adapters/repositories/patient-prisma-repository.js
+++ backend/src/adapters/repositories/patient-prisma-repository.js
@@ -6,12 +6,30 @@ export default class PatientPrismaRepository {
   async findAllPatients() {
     try {
       const patients = await this.prismaClient.patients.findMany({});
-      if (patients.length === 0 || !patients)
-        return [null, "There are not patients fetched"];
+      if (patients.length === 0 || !patients) {
+        return [null, "There are no patients fetched"];
+      }
       return [patients, null];
     } catch (error) {
       throw new Error(
-        `there was a error in patient-prisma-repository.findAllPatient ${error.message}`
+        `There was an error in patient-prisma-repository.findAllPatients: ${error.message}`
+      );
+    }
+  }
+
+  async findPatientById(patientid) {
+    try {
+      const patient = await this.prismaClient.patients.findUnique({
+        where: { id: patientid },
+      });
+      if (!patient) {
+        return [null, "Patient not found"];
+      }
+      return [patient, null];
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        `There was an error in patient-prisma-repository.findPatientById: ${error.message}`
       );
     }
   }
@@ -23,8 +41,38 @@ export default class PatientPrismaRepository {
       });
       return [patient, null];
     } catch (error) {
+      console.error(error);
       throw new Error(
-        `there was a error in patient-prisma-repository.createNewPatient ${error.message}`
+        `There was an error in patient-prisma-repository.createNewPatient: ${error.message}`
+      );
+    }
+  }
+
+  async updatePatient(patientid, updatePatientPayload) {
+    try {
+      const patient = await this.prismaClient.patients.update({
+        where: { id: patientid },
+        data: updatePatientPayload,
+      });
+      return [patient, null];
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        `There was an error in patient-prisma-repository.updatePatient: ${error.message}`
+      );
+    }
+  }
+
+  async deletePatient(patientid) {
+    try {
+      const patient = await this.prismaClient.patients.delete({
+        where: { id: patientid },
+      });
+      return [patient, null];
+    } catch (error) {
+      console.error(error);
+      throw new Error(
+        `There was an error in patient-prisma-repository.deletePatient: ${error.message}`
       );
     }
   }
