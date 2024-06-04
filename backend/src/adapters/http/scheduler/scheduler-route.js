bae9d9ccf8f794663a001db8754d1bae2a9883dc
Omar Enrique Rodríguez Mantilla <omarmr7214@gmail.com>
2024-06-04 18:29:23 -0500

    routes schedulers created

---
+++ backend/src/adapters/http/scheduler/scheduler-route.js
@@ -0,0 +1,36 @@
+import { schedulerHandler } from "../../../utils/intances-usecases.js";
+
+const routes = [
+  {
+    url: "/scheduler-date",
+    method: "GET",
+    handler: schedulerHandler.findSchedulersByDate,
+  },
+  {
+    url: "/scheduler",
+    method: "GET",
+    handler: schedulerHandler.findAllSchedulers,
+  },
+  {
+    url: "/scheduler/:id",
+    method: "GET",
+    handler: schedulerHandler.findSchedulerById,
+  },
+  {
+    url: "/scheduler",
+    method: "POST",
+    handler: schedulerHandler.createNewScheduler,
+  },
+  {
+    url: "/scheduler/:id",
+    method: "PATCH",
+    handler: schedulerHandler.updateScheduler,
+  },
+  {
+    url: "/scheduler/:id",
+    method: "DELETE",
+    handler: schedulerHandler.deleteScheduler,
+  },
+];
+
+export default routes;
