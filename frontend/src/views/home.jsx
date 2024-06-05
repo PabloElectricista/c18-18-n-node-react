07d1d1380b30f34edcccdbd826ec8576154f1b09
Pablo Spector <pablo_spector@hotmail.com>
2024-06-05 04:13:17 -0300

    correccion de error por footer

--- frontend/src/views/home.jsx
+++ frontend/src/views/home.jsx
@@ -1,7 +1,7 @@
 import './home.css'
-import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 import Main from "../components/main/main"
+import Footer from '../components/footer/Footer'
 
 const Home = () => {
     return (
