a21ea6bd2d113277bf3c2daa6e84f8097f4eb4c5
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-04 21:13:02 -0400

    (rizen)edit:pdfRender, pdfpintado(pdf dinamico y estilo 25%)

--- frontend/src/components/pdfRenderer/pdfRenderer.jsx
+++ frontend/src/components/pdfRenderer/pdfRenderer.jsx
@@ -9,32 +9,45 @@ import {
 
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     backgroundColor: '#E4E4E4',
+    padding: 30,
   },
   section: {
     margin: 10,
     padding: 10,
     flexGrow: 1,
   },
+  text: {
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  image: {
+    width: 100,
+    height: 'auto',
+    marginBottom: 10,
+  }
 });
 
 // 1 cm = 28.35 pt
-const pdfRenderer = () => {
-
+const PdfRenderer = ({ paciente, medico, clinica, fechaCita }) => {
   return (
     <Document>
-      <Page size={{ width: 300, height: 400 }} style={styles.page}>
+      <Page size={{ width: 567, height: 425.25 }} style={styles.page}>
         <View style={styles.section}>
-          <Text>¡Hola Mundo!</Text>
-          <Text>
-            Una cartilla médica es un documento que contiene información detallada sobre la historia clínica de un paciente, incluyendo datos personales, antecedentes médicos, tratamientos recetados, resultados de exámenes médicos, entre otros. Es utilizado por los profesionales de la salud para llevar un registro completo de la salud de un paciente y proporcionar un mejor cuidado médico.
-          </Text>
-          <Image src="https://react-pdf.org/images/logo.png" />
+          <Text style={styles.text}>Agenda Salud</Text>
+          <Image style={styles.image} src="https://res.cloudinary.com/dqh2illb5/image/upload/v1717548343/login/logo_mdd2jm.png" />
+          <Text style={styles.text}>DNI: {paciente.patient_dni}</Text>
+          <Text style={styles.text}>Nombre: {paciente.name} {paciente.last_name}</Text>
+          <Text style={styles.text}>Clínica: {clinica.name_clinic}</Text>
+          <Text style={styles.text}>Sala: {clinica.room_number}</Text>
+          <Text style={styles.text}>Fecha de la Cita: {fechaCita}</Text>
+          <Text style={styles.text}>Médico: {medico.name} {medico.last_name}</Text>
+          <Text style={styles.text}>Especialidad: {medico.specialty}</Text>
         </View>
       </Page>
     </Document>
   );
 }
 
-export default pdfRenderer;
+export default PdfRenderer;
--- frontend/src/components/pdfRenderer/pdfpintado.jsx
+++ frontend/src/components/pdfRenderer/pdfpintado.jsx
@@ -1,14 +1,43 @@
-import PdfRenderer from "./pdfRenderer"
-import { PDFDownloadLink } from "@react-pdf/renderer"
+import PdfRenderer from "./pdfRenderer";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import React from 'react';
+
+const paciente = {
+  name: "Omar",
+  last_name: "Rodriguez",
+};
+
+const medico = {
+  name: "Michaela",
+  last_name: "Quinn",
+  specialty: "Medicina general",
+};
+
+const clinica = {
+  name_clinic: "Clínica Sandia",
+  room_number: "101",
+}
+
+const fechaDeLaCita = "30-30-2030"; 
+
+const fecha = new Date().toLocaleDateString().replace(/\//g, '-');
+const fileName = `${paciente.name}-${paciente.last_name}_${fecha}.pdf`;
 
 const Pdfpintado = () => {
   return (
     <div>
-      <PDFDownloadLink document={<PdfRenderer />} fileName="example.pdf">
-        {
-          ({ loading, url, error, blob }) => loading ? 
-          <button>Cargando...</button>
-          : <button>Descargar</button>
+      <PDFDownloadLink
+        document={<PdfRenderer
+          paciente={paciente}
+          medico={medico}
+          clinica={clinica}
+          fechaCita={fechaDeLaCita}
+        />}
+        fileName={fileName}
+      >
+        {({ loading }) => loading ? 
+          <button>Cargando...</button> : 
+          <button>Descargar</button>
         }
       </PDFDownloadLink>
     </div>
