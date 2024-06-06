e2e8c3d08558be8f133644aa1d83ad2138052326
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-06 14:11:15 -0400

    (rigoberto)edit:buttonPayPal.jsx(f createOrder)

--- frontend/src/components/pagos/payPal/buttonPayPal.jsx
+++ frontend/src/components/pagos/payPal/buttonPayPal.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
 const PaypalButton = (props) => {
-    return (
-        <div>
-        </div>
-    )
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          description: props.invoice, //factura
+          amount: { // el monto de la factura
+            value: props.totalValue,
+          },
+        },
+      ]
+    })
+  };
+  return (
+    <div>
+    </div>
+  )
 }
 
 export default PaypalButton;
