2e61e539b0d3609bf91ee2e2562de6ee901f7c25
MauricioAguilarPardo90 <mauricioaguilarpardo@gmail.com>
2024-06-07 17:53:29 -0500

    agregar specialtyRoutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,14 +6,14 @@ const fastify = Fastify({ logger: true });
 
 
 import registerRoutes from "../src/utils/functions/register-routes.js";
-
-import patientRoutes from "../../backend/src/adapters/http/user/patient-route.js";
-import appointmentRoutes from "../../backend/src/adapters/http/appointment/appointment-route.js";
-import schedulerRoutes from "../../backend/src/adapters/http/scheduler/scheduler-route.js";
-
-
 import patientRoutes from "../../backend/src/adapters/http/user/patient-route.js";
 import doctorRoutes from "../../backend/src/adapters/http/user/doctor-route.js";
+import appointmentRoutes from "../../backend/src/adapters/http/appointment/appointment-route.js";
+import schedulerRoutes from "../../backend/src/adapters/http/scheduler/scheduler-route.js";
+import specialtyRoutes from "../../backend/src/adapters/http/user/specialty-route.js";
+
+
+
 
 
 fastify.register(fastifyCors, {
@@ -38,6 +38,7 @@ registerRoutes(fastify, patientRoutes);
 registerRoutes(fastify, doctorRoutes);
 registerRoutes(fastify, appointmentRoutes);
 registerRoutes(fastify, schedulerRoutes);
+registerRoutes(fastify, specialtyRoutes);
 
 
 const start = async () => {
