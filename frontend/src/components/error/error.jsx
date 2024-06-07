c2ff47cfe11cbcda0a4efed7cf9a6f17203d3269
Viper292 <crazymelon@hotmail.es>
2024-06-07 20:05:46 -0300

    Implements styles and functionality

--- frontend/src/components/error/error.jsx
+++ frontend/src/components/error/error.jsx
@@ -1,14 +1,24 @@
-import { useRouteError } from "react-router-dom"
+import { useNavigate, useRouteError } from "react-router-dom"
+import "./error.css"
 
-function Error () {
+function Error() {
   const error = useRouteError()
+  const navigate = useNavigate()
+  const handleNavigate = () => {
+    navigate('/')
+  }
   return (
-    <div>
-      <h1>No se pudo acceder este sitio web</h1>
-      <p>Comprueba si hubo un error en la petición</p>
-      <p>Si no la hubo, prueba ejecutar el diagnóstico de red de tu sistema operativo.</p>
-      <p>{error.statusText || error.message}</p>
-      <button onClick={() => window.location.reload()}>Volver a Cargar</button>
+    <div className="error-wrapper">
+      <div className="error-container">
+        <h1>No se pudo acceder este sitio web</h1>
+        <p className="error-text">Comprueba si hubo un error en la petición</p>
+        <p className="error-text">Si no la hubo, prueba ejecutar el diagnóstico de red de tu sistema operativo.</p>
+        <p className="error-text">Error: {error.statusText || error.message}</p>
+        <div className="button-container">
+          <button className="error-button" onClick={() => window.location.reload()}>Volver a Cargar</button>
+          <button className="error-button" onClick={handleNavigate}>Ir a Inicio</button>
+        </div>
+      </div>
     </div>
   )
 }
