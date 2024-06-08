d64ac04656429fee5fcf35ae09cfbe4cd31ff657
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-08 04:43:10 -0400

    (Rigoberto)edit:Services y su css(se finaliza esta rama dejando el estilo de Main y Services responsivo al 90%)

--- frontend/src/components/main/services/Services.jsx
+++ frontend/src/components/main/services/Services.jsx
@@ -1,9 +1,12 @@
+import React, { useState, useEffect } from "react";
 import img3 from "../../../assets/main/img3.png";
 import img4 from "../../../assets/main/img4.png";
 import "./services.css";
 
 const Services = () => {
-  return (
+  const [isMobile, setIsMobile] = useState(false);
+
+  const DesktopServices = (
     <div className='containerServices'>
       <div className='bloqueServices'>
         <div className='bloqueservicesInt'>
@@ -22,7 +25,45 @@ const Services = () => {
         </div>
       </div>
     </div>
-  )
+  );
+
+ 
+  const MobileServices = (
+    <div className='containerServicesMobile'>
+      <div className='bloqueServicesMobile'>
+        <img className='imgMainMobile' src={img3} alt='fotoservicio' />
+        <div className='bloqueservicesIntMobile'>
+          <span className='spanServicesMobile'>
+            Conectarte con los mejores profesionales en todo momento, a toda hora desde la comodidad de tu hogar.
+          </span>
+        </div>
+      </div>
+      <div className='bloqueServicesMobile'>
+        <img className='imgMainMobile' src={img4} alt='fotoservicio' />
+        <div className='bloqueservicesIntMobile'>
+          <span className='spanServicesMobile'>
+            Ayudarte a gestionar tus turnos y los de tu familia de una forma más sencilla, sin filas ni demoras.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    handleResize(); // func para establecer el estado inicial
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+ 
+
+  // Contenido según pantalla
+  return isMobile ? MobileServices : DesktopServices;
 }
 
-export default Services
+export default Services;
