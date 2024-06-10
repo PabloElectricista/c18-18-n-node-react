8bc2c38ec72da17fdb3aa78ae24cd8cd18f4ace2
Viper292 <crazymelon@hotmail.es>
2024-06-10 17:04:23 -0300

    Adds specialty and doctor "getAll" methods

--- frontend/src/redux/slices/doctorSlices.js
+++ frontend/src/redux/slices/doctorSlices.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { getAllClinics } from '../thunks/doctorThunk'
+import { getAllClinics, getAllSpecialties, getAllDoctors } from '../thunks/doctorThunk'
 
 export const doctorSlice = createSlice({
   name: 'doctor',
@@ -7,6 +7,8 @@ export const doctorSlice = createSlice({
     loading: false,
     error: null,
     clinics: [],
+    specialties: [],
+    doctors: [],
   },
   reducers: {},
   extraReducers: (builder) => {
@@ -20,6 +22,30 @@ export const doctorSlice = createSlice({
         state.clinics = action.payload
       })
       .addCase(getAllClinics.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload
+      })
+      .addCase(getAllSpecialties.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(getAllSpecialties.fulfilled, (state, action) => {
+        state.loading = false
+        state.clinics = action.payload
+      })
+      .addCase(getAllSpecialties.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload
+      })
+      .addCase(getAllDoctors.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(getAllDoctors.fulfilled, (state, action) => {
+        state.loading = false
+        state.clinics = action.payload
+      })
+      .addCase(getAllDoctors.rejected, (state, action) => {
         state.loading = false
         state.error = action.payload
       })
--- frontend/src/redux/thunks/doctorThunk.js
+++ frontend/src/redux/thunks/doctorThunk.js
@@ -4,14 +4,47 @@ import axios from "axios"
 export const getAllClinics = createAsyncThunk(
   'doctor/getAllClinics', 
   async (_, { rejectWithValue }) => {
-  try {
-    const response = await axios.get('/clinic')
-    if (response.data.message === 'success') {
-      return response.data.data
-    } else {
-      return rejectWithValue(response.data.errors)
+    try {
+      const response = await axios.get('/clinic')
+      if (response.data.message === 'success') {
+        return response.data.data
+      } else {
+        return rejectWithValue(response.data.errors)
+      }
+    } catch (err) {
+      return rejectWithValue(err.message)
     }
-  } catch (err) {
-    return rejectWithValue(err.message)
   }
-})
+)
+
+export const getAllSpecialties = createAsyncThunk(
+  'doctor/getAllSpecialties',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/specialty')
+      if (response.data.message === 'success') {
+        return response.data.data
+      } else {
+        return rejectWithValue(response.data.errors)
+      }
+    } catch (err) {
+      return rejectWithValue(err.message)
+    }
+  }
+)
+
+export const getAllDoctors = createAsyncThunk(
+  'doctor/getAllDoctors',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/doctor')
+      if (response.data.message === 'success') {
+        return response.data.data
+      } else {
+        return rejectWithValue(response.data.errors)
+      }
+    } catch (err) {
+      return rejectWithValue(err.message)
+    }
+  }
+)
