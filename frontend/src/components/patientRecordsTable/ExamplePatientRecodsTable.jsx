02831a28a25602a5739943b65ae93d2bfeba3870
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-12 01:30:09 -0400

    (Rigoberto)edit:PatientRecordsTable(ajuste en ruta de appointmentsSlice para prueba)

--- frontend/src/components/patientRecordsTable/ExamplePatientRecodsTable.jsx
+++ frontend/src/components/patientRecordsTable/ExamplePatientRecodsTable.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-//import { fetchAppointments } from "../redux/appointmentsSlice"; // acción para obtener dataCitas
+//import { fetchAppointments } from "../../redux/slices/appointmentsSlice"; // acción para obtener dataCitas
 import PatientRecordsTable from "./PatientRecordsTable";
 
 const ExamplePatientRecordsTable = () => {
