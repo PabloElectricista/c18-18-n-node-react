ea9d5518f670b7f6ca792b0add7bc12b2c9c5292
Franco De Vincentis <devincentisf35@gmail.com>
2024-06-13 04:12:52 -0300

    funcion creada de recovery password

--- backend/src/adapters/http/auth/auth-handler.js
+++ backend/src/adapters/http/auth/auth-handler.js
@@ -1,20 +1,35 @@
-module.exports = class AuthHandler {
-  constructor() {}
+import { emailValidations } from "../../../utils/functions/input-validations.js";
+export default class AuthHandler {
+  //clase creada franco
+  constructor(authUseCases) {
+    this.authUseCases = authUseCases;
+  }
 
-  register = async (req, res) => {
+  recoveryPassword = async (req, res) => {
     try {
-      const errors = registerValidation(req.body);
+      const errors = emailValidations(req.body);
       if (errors)
         return res.status(400).send({
           message: "fail",
           errors,
         });
-      const [user, status, err] = await this.usecases.register(req.body);
-      if (err) return err;
+      const { email } = req.body;
+      const [user, status, err] = await this.authUseCases.recoveryPassword(email);
+      if (err)
+        return res.status(status).send({
+          message: "fail",
+          errors: err,
+        });
       return res.status(status).send({
-        data: user,
         message: "success",
+        data: `The password has already been sent to the email: ${user.email}`,
       });
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        message: "There was internal server error",
+        errors: error,
+      });
+    }
   };
-};
+}
