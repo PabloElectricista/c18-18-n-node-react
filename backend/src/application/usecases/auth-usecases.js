50fdf75760058593750a0e2804d2cdfb68c01c2e
MauricioAguilarPardo90 <mauricioaguilarpardo@gmail.com>
2024-06-13 01:22:17 -0500

    se creo uso de caso auth

---
+++ backend/src/application/usecases/auth-usecases.js
@@ -0,0 +1,20 @@
+export default class AuthUseCases {
+    constructor(patientPrismaRepository, tokenUseCases) {
+      this.patientPrismaRepository = patientPrismaRepository;
+      this.tokenUseCases = tokenUseCases;
+    }
+  
+loginPatient = async (loginPatient) => {
+ const [patient, error] = await this.patientPrismaRepository.findPatientByEmail(loginPatient.email);
+ if (error) return [null, 404, error];
+ 
+ if (patient.phone !== loginPatient.phone)
+ return [ null, 400, "contraseña no coincide con el usuario"];
+ 
+ const [token, errors] = await this.tokenUseCases.generateToken(patient.id, patient.role);
+ if (errors) return [null, 400, errors];
+ return [token, patient, 200, null];
+}  
+
+}
+  
