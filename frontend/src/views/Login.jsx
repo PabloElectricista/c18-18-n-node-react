c4bea34eabd7ab865c052eb18189be3c7be4e961
Viper292 <crazymelon@hotmail.es>
2024-06-14 19:07:30 -0300

    Fixes login

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 
   useEffect(() => {
     if (auth.user) {
-      navigate(`/${auth.user.role}`)
+      navigate(`/${auth.user.role === 'PATIENT' ? 'patient' : 'clinic'}`)
     } else if (auth?.error) {
       toast.error(auth.error, {
         toastId: 'error1',
